87f684a8c589196e4f18ce615bf873fa5a2f8a76
Ricardo Lopes <ricflopes@gmail.com>
2013-12-03 17:34:20 +0000

    Initial project commit. Folder adn files structure, main class

---
+++ src/Master/TCPServer.java
@@ -0,0 +1,7 @@
+package Master;
+
+/**
+ * Created by rlopes on 12/3/13.
+ */
+public class TCPServer {
+}
---
+++ src/Servo/TCPClient.java
@@ -0,0 +1,7 @@
+package Servo;
+
+/**
+ * Created by rlopes on 12/3/13.
+ */
+public class TCPClient {
+}
