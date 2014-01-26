d0beb7f7f0e575d2bf59605c77b9f512855755f1
Ricardo Lopes <ricflopes@gmail.com>
2014-01-26 18:34:53 +0000

    Servo clock model implemented

---
+++ src/Servo/ServoClock.java
@@ -0,0 +1,51 @@
+package Servo;
+
+public class ServoClock extends Thread{
+	private double a; 	//skew
+	private double T;	//Periodo de sincronização
+	public  double slave_clock;
+	
+	/**
+	 *  Construtor do relógio do servodor, inicializa o contador a zero
+	 *  e define o skew e o periodo de sincronizacao 
+	 * @param a Skew	
+	 * @param T Periodo de sincronizacao
+	 */
+	public ServoClock(double a, double T) {
+		this.setA(a);
+		this.setT(T);
+		this.slave_clock = 0.0;
+	}
+	
+	public void run(){
+		do{
+		slave_clock += (a*T);//TODO Efectuar contas em nano/milisegundos
+		}while(true);
+	}
+	
+	public double getSlaveClock(){
+		return slave_clock;
+	}
+	
+	public void reset(){
+		this.setA(0.0);
+		this.setT(0.0);
+		this.slave_clock = 0.0;
+	}
+
+	public double getA() {
+		return a;
+	}
+
+	public void setA(double a) {
+		this.a = a;
+	}
+
+	public double getT() {
+		return T;
+	}
+
+	public void setT(double t) {
+		this.T = t;
+	}
+}
