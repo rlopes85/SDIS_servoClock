e9a727f8046e43defdfe07a2b3b42446d8b08c1f
Ricardo Lopes <ricflopes@gmail.com>
2014-01-30 16:01:11 +0000

    corrected clock model

--- src/Servo/ServoClock.java
+++ src/Servo/ServoClock.java
@@ -18,22 +18,14 @@ public class ServoClock extends Thread{
 	}
 	@Override
 	public void run(){
-        System.out.println("clock run");
-        int i = 0;
 		do{
-            if (i < 10){
-                i++;
-                try {
-                    sleep(100);
-                } catch (InterruptedException e) {
-                    System.out.println("erro na contagem relogio");
-                    e.printStackTrace();
-                }
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("erro na contagem relogio");
+                e.printStackTrace();
             }
-		    else{
-                slave_clock += (a*T);
-                i = 0;
-            }
+            slave_clock += (a*T);
 		}while(true);
 
 	}
